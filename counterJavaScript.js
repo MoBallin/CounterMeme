3fd3409806d404cd5eba52c8651d9be7191ded72
Mohammed Diab <mohammeddiab@Mohammeds-MacBook-Pro.local>
2017-11-01 16:50:41 -0500

    Update sign-in task

--- counterJavaScript.js
+++ counterJavaScript.js
@@ -34,3 +34,76 @@ $(function(){
 		evaluatePath(location.pathname); 
 	}); 
 }); 
+
+function field_focus(field, fname)
+  {
+    if(field.placeholder == placeholder)
+    {
+      field.placeholder = '';
+    }
+  }
+
+function field_focus(field, lname)
+  {
+    if(field.placeholder == placeholder)
+    {
+      field.placeholder = '';
+    }
+  }
+
+function field_focus(field, username)
+  {
+    if(field.placeholder == placeholder)
+    {
+      field.placeholder = '';
+    }
+  }
+
+  function field_blur(field, username)
+  {
+    if(field.placeholder == '')
+    {
+      field.placeholder = placeholder;
+    }
+  }
+
+function field_focus(field, password)
+  {
+    if(field.placeholder == placeholder)
+    {
+      field.placeholder = '';
+    }
+  }
+
+  function field_blur(field, password)
+  {
+    if(field.placeholder == '')
+    {
+      field.placeholder = placeholder;
+    }
+  }
+
+function field_focus(field, confirmedPass)
+  {
+    if(field.placeholder == placeholder)
+    {
+      field.placeholder = '';
+    }
+  }
+
+  function field_blur(field, confirmedPass)
+  {
+    if(field.placeholder == '')
+    {
+      field.placeholder = placeholder;
+    }
+  }
+//Fade in dashboard box
+$(document).ready(function(){
+    $('.box').hide().fadeIn(1000);
+    });
+
+//Stop click event
+$('a').click(function(event){
+    event.preventDefault(); 
+	});
